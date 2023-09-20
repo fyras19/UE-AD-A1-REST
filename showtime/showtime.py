019eeed2faf705297d6c0d613d5f97a25c87a7d6
Firas Yahyaoui <firas.yahyaoui@imt-atlantique.net>
2023-09-20 11:34:22 +0200

    Complete showtime microservice

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -14,6 +14,17 @@ with open('{}/databases/times.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
 
+@app.route("/showtimes", methods=['GET'])
+def showtimes():
+   return make_response(jsonify({"schedule": schedule}))
+
+@app.route("/showmovies/<date>", methods=['GET'])
+def showmoviesByDate(date):
+   for day in schedule:
+      if day["date"] == date:
+         return make_response(jsonify(day))
+   return make_response(jsonify({"error": "Date not found!"}))
+
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+   app.run(host=HOST, port=PORT, debug=True)
