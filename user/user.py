d9540e6bea8427f7cd0f008e887361966572d475
Aymanjeb <aymanjeb02@gmail.com>
2023-10-25 08:35:50 +0200

    a

--- user/user.py
+++ user/user.py
@@ -11,6 +11,9 @@ HOST = '0.0.0.0'
 with open('{}/databases/users.json'.format("."), "r") as jsf:
    users = json.load(jsf)["users"]
 
+
+###### Users part ##########
+
 @app.route("/", methods=['GET'])
 def home():
    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
@@ -26,16 +29,6 @@ def get_user_byid(userid):
             return make_response(jsonify(user),200)
     return make_response(jsonify({"error":"User ID not found"}),400)
 
-@app.route("/bookings/<userid>", methods=['GET'])
-def get_booking_for_user(userid):
-   response = requests.get(f"http://{HOST}:3201/bookings/{userid}")
-   return make_response(response.json(), response.status_code)
-
-@app.route("/movies", methods=['GET'])
-def get_movies():
-   response = requests.get(f"http://{HOST}:3200/json")
-   return make_response(response.json(), response.status_code)
-
 @app.route("/users/<userid>", methods=['POST'])
 def add_user(userid):
    req = request.get_json()
@@ -46,13 +39,55 @@ def add_user(userid):
    res = make_response(jsonify(req),200)
    return res
 
+###### Bookings part ##########
+
+@app.route("/bookings/<userid>", methods=['GET'])
+def get_booking_for_user(userid):
+   response = requests.get(f"http://{HOST}:3201/bookings/{userid}")
+   return make_response(response.json(), response.status_code)
+
 @app.route("/bookings/<userid>", methods=['POST'])
 def add_booking_byuser(userid):
    req = request.get_json()
    response = requests.post(f"http://{HOST}:3201/bookings/{userid}",json=req)
    return make_response(response.json(), response.status_code)
 
+###### Movies part ##########
 
+@app.route("/movies", methods=['GET'])
+def get_movies():
+   response = requests.get(f"http://{HOST}:3200/json")
+   return make_response(response.json(), response.status_code)
+
+@app.route("/movies/<movieid>", methods=['GET'])
+def get_movie_byid(movieid):
+   response = requests.get(f"http://{HOST}:3200/movies/{movieid}")
+   return make_response(response.json(), response.status_code)
+
+@app.route("/titles", methods=['GET'])
+def get_movie_bytitle():
+   title = request.args.get('title')
+   response = requests.get(f"http://{HOST}:3200/titles", params={'title': title})
+   return make_response(response.json(), response.status_code)
+
+@app.route("/movies/<movieid>", methods=['POST'])
+def create_movie(movieid):
+   req = request.get_json()
+   response = requests.post(f"http://{HOST}:3200/movies/{movieid}", json=req)
+   return make_response(response.json(), response.status_code)
+
+@app.route("/movies/<movieid>", methods=['DELETE'])
+def del_movie(movieid):
+   response = requests.delete(f"http://{HOST}:3200/movies/{movieid}")
+   return make_response(response.json(), response.status_code)
+
+@app.route("/movies/<movieid>/<rate>", methods=['PUT'])
+def update_movie_rating(movieid, rate):
+   response = requests.put(f"http://{HOST}:3200/movies/{movieid}/{rate}")
+   return make_response(response.json(), response.status_code)
+
+
+###### Run App ##########
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
